0fa44110ee235046f054f3133803b403365db6f6
cainhuang <cain.huang@outlook.com>
2021-10-05 22:31:47 +0800

    add libraries

---
+++ libs/utils/include/utils/trap.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2021 The Open Media Rendering Engine Project
+ * Copyright (C) 2021 cain.huang@outlook.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OMRE_UTILS_TRAP_H_
+#define OMRE_UTILS_TRAP_H_
+
+#include <csignal>
+
+#if defined(WIN32)
+#include <Windows.h>
+#include <utils/unwindows.h>
+#endif
+
+namespace utils {
+
+    // This can be used as a programmatic breakpoint.
+    inline void debug_trap() noexcept {
+#if defined(WIN32)
+        DebugBreak();
+#else
+        std::raise(SIGINT);
+#endif
+    }
+    
+} // namespace utils
+
+#endif //OMRE_UTILS_TRAP_H_
